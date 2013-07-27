02962c15ac3cd216a6a94e412982e55e99f78686
Richard <richard.tetaz@gmail.com>
2013-07-27 12:45:49 +0200

    fix angular

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,15 @@
-console.log('This would be the main JS file.');
+myApp = angular.module("myApp",[]);
+myApp.controller("myCtrl", function($scope){
+	$scope.a1 = true;
+	$scope.a2 = true;
+	$scope.a3 = {
+		show: true,
+		gist: 1
+	}
+	$scope.a4 = {
+		show: true
+	}
+	$scope.a5 = {
+		show: true
+	}
+});
