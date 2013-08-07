8409bf62818a6be982ffd58ad5bc5c39f34cf87c
Richard <richard.tetaz@gmail.com>
2013-08-07 22:35:28 +0200

    [MAJ] Test avec le all ng-include loader

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,31 +1,5 @@
 myApp = angular.module("myApp", []);
 myApp.controller("myCtrl", function($scope){
-	$scope.a1 = true;
-	$scope.a2 = true;
-	$scope.a3 = {
-		show: true,
-		gist: 1
-	};
-	$scope.a4 = {
-		show: true
-	};
-	$scope.a5 = {
-		show: true
-	};
-	$scope.a6 = {
-		show: true,
-		value: '',
-		save: ''
-	};
-	$scope.a7 = {
-		show: true
-	};
-	$scope.a8 = {
-		show: true
-	};
-	$scope.a9 = {
-		show: true
-	};
 	$scope.articles = [];
 	$scope.articles.push({url: 'articles/9.html', title: "Quiche a la ratatouille"});
 	$scope.articles.push({url: 'articles/8.html', title: "Yoh man !"});
@@ -36,6 +10,11 @@ myApp.controller("myCtrl", function($scope){
 	$scope.articles.push({url: 'articles/3.html', title: "Quelques directives Angular"});
 	$scope.articles.push({url: 'articles/2.html', title: "Ma configuration pour Sublime Text"});
 	$scope.articles.push({url: 'articles/1.html', title: "Le BOLD c'est la vie"});
+
+	$scope.$on('$includeContentLoaded', function(event) {
+	  console.log('another include was loaded', event.targetScope);
+	  $('pre code').each(function(i, e) { hljs.highlightBlock(e); });
+	});
 });
 
 myApp.directive('ngBlur', function() {
