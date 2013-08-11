68e4eaa7f8b43f56aae9894b9efc73ad4d97be85
Richard <richard.tetaz@gmail.com>
2013-08-11 17:26:32 +0200

    [ADD] Markdwon

--- javascripts/main.js
+++ javascripts/main.js
@@ -27,3 +27,21 @@ myApp.directive('ngBlur', function() {
 		});
 	};
 });
+
+myApp.directive('markdown', function () {
+	var converter = new Showdown.converter();
+	return {
+		restrict: 'AE',
+		link: function (scope, element, attrs) {
+			if (attrs.btfMarkdown) {
+				scope.$watch(attrs.btfMarkdown, function (newVal) {
+					var html = converter.makeHtml(newVal);
+					element.html(html);
+				});
+			} else {
+				var html = converter.makeHtml(element.text());
+				element.html(html);
+			}
+		}
+	};
+});
