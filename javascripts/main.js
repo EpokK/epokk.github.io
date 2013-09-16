b21111d76d03786bed1011236acfd8a79aa1e2b0
Richard <richard.tetaz@gmail.com>
2013-09-16 22:15:33 +0200

    [FIX] de [FIX]

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,18 +2,18 @@ myApp = angular.module("myApp", []);
 myApp.controller("myCtrl", function($scope){
 	var countNgIncludLoaded = 0;
 	$scope.articles = [];
-	$scope.articles.push({show: false, url: 'articles/12.md', title: "Soupe a la creme de poireaux"});
-	$scope.articles.push({show: false, url: 'articles/11.md', title: "Moment.js"});
-	// $scope.articles.push({show: false, url: 'articles/10.md', title: "Transclude tu connais ?"});
-	$scope.articles.push({show: false, url: 'articles/9.html', title: "Quiche a la ratatouille"});
-	$scope.articles.push({show: false, url: 'articles/8.html', title: "Yoh man !"});
-	$scope.articles.push({show: false, url: 'articles/7.html', title: "Pates a la Carbonara"});
-	$scope.articles.push({show: false, url: 'articles/6.html', title: "Input like Gmail contact"});
-	$scope.articles.push({show: false, url: 'articles/5.html', title: "Tarte aux mirabelles"});
-	$scope.articles.push({show: false, url: 'articles/4.html', title: "Ractive.js : Alternative a AngularJS"});
-	$scope.articles.push({show: false, url: 'articles/3.html', title: "Quelques directives Angular"});
-	$scope.articles.push({show: false, url: 'articles/2.html', title: "Ma configuration pour Sublime Text"});
-	$scope.articles.push({show: false, url: 'articles/1.html', title: "Le BOLD c'est la vie"});
+	$scope.articles.push({url: 'articles/12.md', title: "Soupe a la creme de poireaux"});
+	$scope.articles.push({url: 'articles/11.md', title: "Moment.js"});
+	// $scope.articles.push({url: 'articles/10.md', title: "Transclude tu connais ?"});
+	$scope.articles.push({url: 'articles/9.html', title: "Quiche a la ratatouille"});
+	$scope.articles.push({url: 'articles/8.html', title: "Yoh man !"});
+	$scope.articles.push({url: 'articles/7.html', title: "Pates a la Carbonara"});
+	$scope.articles.push({url: 'articles/6.html', title: "Input like Gmail contact"});
+	$scope.articles.push({url: 'articles/5.html', title: "Tarte aux mirabelles"});
+	$scope.articles.push({url: 'articles/4.html', title: "Ractive.js : Alternative a AngularJS"});
+	$scope.articles.push({url: 'articles/3.html', title: "Quelques directives Angular"});
+	$scope.articles.push({url: 'articles/2.html', title: "Ma configuration pour Sublime Text"});
+	$scope.articles.push({url: 'articles/1.html', title: "Le BOLD c'est la vie"});
 
 	$scope.$on('$includeContentLoaded', function(event) {
 		countNgIncludLoaded++;
