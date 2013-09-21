a6a66a382574e7051ccd342f844aa33139806f80
Richard <richard.tetaz@gmail.com>
2013-09-21 11:19:50 +0200

    [ADD] Debut de la partie CV

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,6 +2,9 @@ myApp = angular.module("myApp", []);
 myApp.controller("myCtrl", function($scope){
 	var countNgIncludLoaded = 0;
 	$scope.articles = [];
+	$scope.cv = {
+		show: false
+	};
 	$scope.articles.push({url: 'articles/12.md', title: "Soupe a la creme de poireaux"});
 	$scope.articles.push({url: 'articles/11.md', title: "Moment.js"});
 	// $scope.articles.push({url: 'articles/10.md', title: "Transclude tu connais ?"});
