91f3bb71a645a2eb71e610d8f6b3c5b02dd36f7a
Richard <richard.tetaz@gmail.com>
2013-09-30 22:32:56 +0200

    [FIX] CV

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ myApp = angular.module("myApp", []);
 myApp.controller("myCtrl", function($scope){
 	var countNgIncludLoaded = 0;
 	$scope.articles = [];
-	$scope.cv = {show: false, url: 'documents/cv.html'};
+	$scope.cv = {show: false, url: ''};
 	$scope.articles.push({url: 'articles/12.md', title: "Soupe a la creme de poireaux"});
 	$scope.articles.push({url: 'articles/11.md', title: "Moment.js"});
 	// $scope.articles.push({url: 'articles/10.md', title: "Transclude tu connais ?"});
@@ -29,7 +29,8 @@ myApp.controller("myCtrl", function($scope){
 	};
 
 	$scope.showCV = function() {
-		$scope.cv.show = true;
+		$scope.cv.show =! $scope.cv.show;
+		$scope.url = 'documents/cv.html';
 	};
 });
 
